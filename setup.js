86f46af0f8fe68a52013b81cc36b72d08a7fe671
David <david.heckhoff@gmail.com>
2021-11-24 22:34:25 +0100

    updated performance

--- setup.js
+++ setup.js
@@ -1,43 +1,57 @@
-let currentPokemon = ''
-let totalPokemon = 0
 let minPokemonIdToShow = 1
-let currentlyShowingAmount
+let maxPokemonIdToShow = 30
+let currentlyShowingAmount = 0
+let allPokemonNames = []
 
-async function fetchSetup(){
-    let url = 'https://pokeapi.co/api/v2/pokemon/'
+async function loadAllNames(){
+    let url = 'https://pokeapi.co/api/v2/pokemon/?limit=1118'
     let response = await fetch(url)
     let responseAsJson = await response.json()
 
-    totalPokemon = responseAsJson.count
-    addPokemonsToShow(20)
-}
-
-async function addPokemonsToShow(amount){
-
-    for (let i = 0; i < amount; i++) {
-        renderPokemon(minPokemonIdToShow + i)
+    for (let i = 0; i < responseAsJson.results.length; i++) {
+        allPokemonNames.push(responseAsJson.results[i])
     }
 
-    minPokemonIdToShow += amount
+    updateSearchPokemons()
 }
 
-async function renderPokemon(id){
-    currentlyShowingAmount += 1
+function updateSearchPokemons() {
+    const renderContainer = document.getElementById('pokedex-list-render-container')
 
+    if(currentlyShowingAmount + minPokemonIdToShow <= maxPokemonIdToShow) {
+        renderSearchPokemon(minPokemonIdToShow + currentlyShowingAmount)
+        currentlyShowingAmount += 1
+    }
+}
+
+async function renderSearchPokemon(id){
     let url = 'https://pokeapi.co/api/v2/pokemon/' + id
     let response = await fetch(url)
     let responseAsJson = await response.json()
+        .then(responseAsJson => {
+            const renderContainer = document.getElementById('pokedex-list-render-container')
 
-    const renderContainer = document.getElementById('pokedex-list-render-container')
-
-    renderContainer.innerHTML +=    `<div class="pokemon-render-result-container container center column">
-                                        <img class="search-pokemon-image" src="${responseAsJson.sprites.versions['generation-v']['black-white'].animated['front_default']}">
-                                        <span class="bold font-size-12">N° ${id}</span>
-                                        <h3>${capitalizeFirstLetter(responseAsJson.name)}</h3>
-                                    </div>`
-
+            renderContainer.innerHTML +=    `<div class="pokemon-render-result-container container center column">
+                                                <img class="search-pokemon-image" src="${responseAsJson.sprites.versions['generation-v']['black-white'].animated['front_default']}">
+                                                <span class="bold font-size-12">N° ${id}</span>
+                                                <h3>${capitalizeFirstLetter(responseAsJson.name)}</h3>
+                                            </div>`
+            
+            updateSearchPokemons()
+        })
 }
 
+
+/** load new pokemons when bottom is reached */
+window.addEventListener('scroll', function(){
+    if(window.scrollY + 100 >= document.documentElement.scrollHeight - document.documentElement.clientHeight) {
+        maxPokemonIdToShow += 30
+        updateSearchPokemons()
+    }
+})
+
+
+/**capitalize first letter (mainly for payload) */
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
